19a38e3263843bddfd5bb5dd5d7f5a8376f1f562
Joshua Pregbaha <jpregbaha@gmail.com>
2021-06-30 19:07:05 +0100

    Uninstalled deprecated body parser

--- src/server.js
+++ src/server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost:27017/blogDB", {
 	useNewUrlParser: true,
